4f60b28b5240535b5ced95e587f3c457bb726e51
jayesh-acl <jayesh.das@acldigital.com>
2024-09-30 13:26:27 +0530

    Update largest_number.py

--- largest_number.py
+++ largest_number.py
@@ -6,6 +6,6 @@ def find_largest_number(lst):
             largest=num
     return largest
 
-lst=[300000,5,4,7,1,2,5,66,4,44,200,2123,4]
+lst=[300000,5,4,7,1,2,5,66,4,44,200,7474,4]
 output=find_largest_number(lst)
 print(f"{output} is largest number in given {lst} list")
